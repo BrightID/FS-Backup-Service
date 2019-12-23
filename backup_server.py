d2c9f023aa9b2f901dff4a5abc5064d1ee15bcba
adamstallard <adam.stallard@gmail.com>
2019-12-23 15:08:22 -0800

    make it REST

--- backup_server.py
+++ backup_server.py
@@ -3,15 +3,12 @@ from os.path import *
 import base64
 from flask import Flask, abort, jsonify, request
 
-DIR = './data/'
+DIR = '/var/opt/recovery-service/'
 
-DIR = join(dirname(abspath(__file__)), DIR)
 app = Flask(__name__)
 
-@app.route('/get', methods=['GET'])
-def get():
-    key1 = request.args.get('key1', '')
-    key2 = request.args.get('key2', '')
+@app.route('/backup/<key1>/<key2>', methods=['GET'])
+def get(key1, key2):
     if not key1 or not key2:
         abort(400)
     path = join(DIR, key1, key2)
@@ -20,10 +17,8 @@ def get():
     with open(path, 'r') as f:
         return f.read()
 
-@app.route('/set', methods=['POST'])
-def set():
-    key1 = request.json.get('key1', '')
-    key2 = request.json.get('key2', '')
+@app.route('/backup/<key1>/<key2>', methods=['PUT'])
+def set(key1, key2):
     data = request.json.get('data', '')
     if not key1 or not key2:
         abort(400)
@@ -36,5 +31,8 @@ def set():
     return jsonify(success=True)
 
 if __name__ == '__main__':
+    DIR = './data/'
+    DIR = join(dirname(abspath(__file__)), DIR)
     if not exists(DIR): mkdir(DIR)
     app.run(host='0.0.0.0', port=5001, threaded=True, debug=True)
+
